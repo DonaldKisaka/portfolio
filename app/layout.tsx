f1ae9766994d131050756058f872d53b8a85f525
DonaldKisaka <kisakadonald1@gmail.com>
2025-04-30 14:38:00 +0300

    Final  commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>
+      <body className={`${inter.variable} antialiased bg-black`}>
         <div className="h-full w-full mx-auto max-w-[47.5rem] p-6">
           <Nav />
           {children}
