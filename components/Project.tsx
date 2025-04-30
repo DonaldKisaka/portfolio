4cb106c63609b2f3f1d06bbe767687ed4d1bd0ec
DonaldKisaka <kisakadonald1@gmail.com>
2025-04-30 15:12:37 +0300

    Final  commit

--- components/Project.tsx
+++ components/Project.tsx
@@ -7,10 +7,10 @@ const Project = () => {
     <div className='flex flex-col w-full mt-24'>
         <h2 id='projects' className='font-bold text-lg tracking-widest text-white uppercase mb-10'>Projects</h2>
         <div className='flex flex-col w-full'>
-            <div className='flex items-start gap-8'>
-                <div className='rounded-lg w-[640px] border border-[#D3D3D3]/50 p-5 flex flex-col gap-5'>
+            <div className='flex items-start gap-8 max-md:flex-col max-md:items-center'>
+                <div className='rounded-lg w-full max-w-[640px] border border-[#D3D3D3]/50 p-3 sm:p-5 flex flex-col gap-5'>
                   <Link target='_blank' aria-label='See more about philip&apos;s website' href="https://pkw.finance/">
-                    <Image src="/philipswebsite.png" alt="project screenshot" loading='lazy' width={600} height={380} className='rounded-md object-cover text-transparent'/> 
+                    <Image src="/philipswebsite.png" alt="project screenshot" loading='lazy' width={600} height={380} className='rounded-md object-cover w-full h-auto text-transparent'/> 
                   </Link>
                   <div className='flex flex-col'>
                     <p className='font-semibold text-white text-xl tracking-widest'>PKW.FINANACE</p>
@@ -38,21 +38,21 @@ const Project = () => {
                            className='text-transparent' />
                            <p className='text-xs whitespace-nowrap'>Shadcn</p>
                         </div>
-                        <div className='flex items-center gap-2 w-full max-md:flex-col'>
-                            <Link target='_blank' className='w-full uppercase text-xs whitespace-nowrap transition-all rounded-full font-bold text-white hover:text-gray-800 flex items-center justify-center px-4 py-2 gap-2 max-md:w-full' href="https://github.com/DonaldKisaka/PKW-corporate">
-                            <Image src="/github-icon.svg" loading="lazy" alt="GitHub" width={20} height={20} className="text-transparent"/>
-                            Source code
-                            </Link>
-                        </div>
+                    </div>
+                    <div className='flex items-center mt-4 w-full'>
+                        <Link target='_blank' className='w-full sm:w-auto uppercase text-xs whitespace-nowrap transition-all rounded-full font-bold text-white hover:text-gray-800 hover:bg-white bg-transparent border border-white flex items-center justify-center px-4 py-2 gap-2' href="https://github.com/DonaldKisaka/PKW-corporate">
+                        <Image src="/github-icon.svg" loading="lazy" alt="GitHub" width={20} height={20} className="text-transparent"/>
+                        Source code
+                        </Link>
                     </div>
                   </div>
                 </div>
             </div>
 
-            <div className='flex items-start gap-8 mt-10'>
-                <div className='rounded-lg w-[640px] border border-[#D3D3D3]/50 p-5 flex flex-col gap-5'>
+            <div className='flex items-start gap-8 mt-10 max-md:flex-col max-md:items-center'>
+                <div className='rounded-lg w-full max-w-[640px] border border-[#D3D3D3]/50 p-3 sm:p-5 flex flex-col gap-5'>
                   <Link target='_blank' aria-label='See more about philip&apos;s website' href="https://furniture-app-utbp.vercel.app/">
-                    <Image src="/RenoTzy.png" alt="project screenshot" loading='lazy' width={600} height={380} className='rounded-md object-cover text-transparent'/> 
+                    <Image src="/RenoTzy.png" alt="project screenshot" loading='lazy' width={600} height={380} className='rounded-md object-cover w-full h-auto text-transparent'/> 
                   </Link>
                   <div className='flex flex-col'>
                     <p className='font-semibold text-white text-xl tracking-widest'>RENOTZY</p>
@@ -85,24 +85,21 @@ const Project = () => {
                            className='text-transparent' />
                            <p className='text-xs whitespace-nowrap'>Shadcn</p>
                         </div>
-                        <div className='flex items-center gap-2 w-full max-md:flex-col'>
-                            <Link target='_blank' className='w-full uppercase text-xs whitespace-nowrap transition-all rounded-full font-bold text-white hover:text-gray-800 flex items-center justify-center px-4 py-2 gap-2 max-md:w-full' href="https://github.com/DonaldKisaka/furniture-app">
-                            <Image src="/github-icon.svg" loading="lazy" alt="GitHub" width={20} height={20} className="text-transparent"/>
-                            Source code
-                            </Link>
-                        </div>
+                    </div>
+                    <div className='flex items-center mt-4 w-full'>
+                        <Link target='_blank' className='w-full sm:w-auto uppercase text-xs whitespace-nowrap transition-all rounded-full font-bold text-white hover:text-gray-800 hover:bg-white bg-transparent border border-white flex items-center justify-center px-4 py-2 gap-2' href="https://github.com/DonaldKisaka/furniture-app">
+                        <Image src="/github-icon.svg" loading="lazy" alt="GitHub" width={20} height={20} className="text-transparent"/>
+                        Source code
+                        </Link>
                     </div>
                   </div>
                 </div>
-
-                
-
             </div>
 
-            <div className='flex items-start gap-8 mt-10'>
-                <div className='rounded-lg w-[640px] border border-[#D3D3D3]/50 p-5 flex flex-col gap-5'>
+            <div className='flex items-start gap-8 mt-10 max-md:flex-col max-md:items-center'>
+                <div className='rounded-lg w-full max-w-[640px] border border-[#D3D3D3]/50 p-3 sm:p-5 flex flex-col gap-5'>
                   <Link target='_blank' aria-label='See more about philip&apos;s website' href="https://insyc.vercel.app/">
-                    <Image src="/Insync.png" alt="project screenshot" loading='lazy' width={600} height={380} className='rounded-md object-cover text-transparent'/> 
+                    <Image src="/Insync.png" alt="project screenshot" loading='lazy' width={600} height={380} className='rounded-md object-cover w-full h-auto text-transparent'/> 
                   </Link>
                   <div className='flex flex-col'>
                     <p className='font-semibold text-white text-xl tracking-widest'>INSYNC</p>
@@ -135,22 +132,17 @@ const Project = () => {
                            className='text-transparent' />
                            <p className='text-xs whitespace-nowrap'>Shadcn</p>
                         </div>
-                        <div className='flex items-center gap-2 w-full max-md:flex-col'>
-                            <Link target='_blank' className='w-full uppercase text-xs whitespace-nowrap transition-all  rounded-full font-bold text-white hover:text-gray-800 flex items-center justify-center px-4 py-2 gap-2 max-md:w-full' href="https://github.com/DonaldKisaka/Insyc">
-                            <Image src="/github-icon.svg" loading="lazy" alt="GitHub" width={20} height={20} className="text-transparent"/>                
-                            Source code
-                            </Link>
-                        </div>
+                    </div>
+                    <div className='flex items-center mt-4 w-full'>
+                        <Link target='_blank' className='w-full sm:w-auto uppercase text-xs whitespace-nowrap transition-all rounded-full font-bold text-white hover:text-gray-800 hover:bg-white bg-transparent border border-white flex items-center justify-center px-4 py-2 gap-2' href="https://github.com/DonaldKisaka/Insyc">
+                        <Image src="/github-icon.svg" loading="lazy" alt="GitHub" width={20} height={20} className="text-transparent"/>                
+                        Source code
+                        </Link>
                     </div>
                   </div>
                 </div>
-
-                
-
             </div>
         </div>
-
-      
     </div>
   )
 }
